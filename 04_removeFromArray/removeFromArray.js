22b0ba3c679f25a75ce648fff538579730171c83
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-02-21 20:43:43 -0300

    finally solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    //const newArray = [];
+    for(let i = 0; i < arr.length; i++) {
+        for(arg of args) {
+            if(arr[i] === arg) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
