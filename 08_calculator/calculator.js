191459467a71560dce861fad45973f4b5d0ee9bc
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-05-01 07:17:16 -0300

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, item) => {
+    acc += item;
+    return acc;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, item) => {
+    acc *= item;
+    return acc;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  };
+  return result;
 };
 
 // Do not edit below this line
