0856564057ddcd2e98f38fc5c2a1c16466de2ff3
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-05-01 08:44:10 -0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Removes all non-alphanumeric characters from the input string and converts it to uppercase.
+    str = str.toUpperCase().replace(/[^\w'"]|_/g, '');
 
+    // Reverses the transformed string.
+    let result = str.split('').reverse().join('');
+
+    // Compares the reversed string with the original transformed string
+    return (result == str);
 };
 
 // Do not edit below this line
