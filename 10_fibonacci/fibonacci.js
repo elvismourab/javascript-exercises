b06c68a8db653dec9d85ef0e9295b740ae0bac31
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-05-01 22:14:44 -0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    index = Number(index);
+    if (index < 0) return 'OOPS';
+    if (index === 0) return 0;
 
+    const x = [];
+    x[0] = 1;
+    x[1] = 1;
+
+    for (let i = 2; i < index; i++) {
+        x[i] = x[i - 1] + x[i - 2];
+    }
+    return x[x.length - 1];
 };
 
 // Do not edit below this line
